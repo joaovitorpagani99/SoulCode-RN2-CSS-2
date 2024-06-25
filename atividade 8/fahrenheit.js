912ba629be659c5cad0dbbad582939a726b423a3
joaovitorpagani99 <joaovitorpagani7@hotmail.com>
2024-06-25 09:15:10 -0300

    sintaxe basica

---
+++ atividade 8/fahrenheit.js
@@ -0,0 +1,7 @@
+let F = 100;         
+
+C = (F - 32) / 1.8;
+
+console.log(C)
+
+
---
+++ atividade 8/media.js
@@ -0,0 +1,18 @@
+let nota1 = 7; 
+let nota2 = 8.5;
+let nota3 = 9; 
+let nota4 = 6; 
+let nota5 = 7.5; 
+
+let peso1 = 3;
+let peso2 = 2;
+let peso3 = 1;
+let peso4 = 4;
+let peso5 = 5;
+
+let somaProdutos = (nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3) + (nota4 * peso4) + (nota5 * peso5);
+let somaPesos = peso1 + peso2 + peso3 + peso4 + peso5;
+
+let mediaPonderada = somaProdutos / somaPesos;
+
+console.log("A média ponderada é:", mediaPonderada);
